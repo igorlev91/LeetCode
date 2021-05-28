dc1c4b21a6b3979ad94fa063591000e6864319f7
Igor <igorlevdansky91@gmail.com>
2021-05-28 13:43:24 +0200

    Add SortArrayByParity second solutions

--- 905. Sort Array By Parity.h
+++ 905. Sort Array By Parity.h
@@ -1,11 +1,49 @@
 Solution {
 public:
-    vector<int> sortArrayByParity(vector<int>& A) {
-        
+    vector<int> sortArrayByParity(vector<int>& a) {
+        vector<int> res;
+        res.reserve(a.size());
+        for (int e : a){
+            if ((e & 1 ) == 0) {
+                res.push_back(e);
+            }
+        }
+
+        for (int e : a) {
+            if ((e & 1) != 0) {
+                res.push_back(e);
+            }
+        }
+        asset(res.size() == a.size());
+        return res; 
     }
 };
 
 Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& a) {
+        int i = 0;
+        int j = (int)a.size() - 1;
+        while (true) {
+            while (i < j && (a[i] & 1) == 0) {
+                i++;
+            }
+            while (i < j && (a[i] & 1) != 0) {
+                j--;
+            }
+            if (i < j) {
+                swap(a[i],a[j]);
+                i++;
+                j--;
+            } else {
+                break;
+            }
+        }
+        return move(a);
+    }
+};
+
+Solution { 
 public:
     vector<int> sortArrayByParity(vector<int>& A) {
         int l = 0; r = A.size() - 1;
@@ -28,4 +66,8 @@ class Solution:
             while l < r and A[l] % 2 == 0: l += 1
             while l < r and A[r] % 2 == 1: r -= 1
             A[l], A[r] = A[r], A[l]
-         return A       
+         return A  
+
+class Solution(object):
+    def sortArrayByParity(self, A):
+    return ([x for x in A if x % 2 == 0] + [x for x in A if x % 2 == 1])
